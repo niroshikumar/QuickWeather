b427fa63683e6688ade0eb02a4e8c5e56297c54a
Niroshi Kumarasinghe Goncha <niroshikumar@gmail.com>
2023-09-28 20:00:54 +0800

    Initial Design

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+document.getElementById('btnDark').addEventListener('click', () => {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+});
+
+document.getElementById('btnLight').addEventListener('click', () => {
+    document.documentElement.setAttribute('data-bs-theme', 'light')
+})
+
+
